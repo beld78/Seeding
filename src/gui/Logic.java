859a3b46da43769f79252e7892943f08e5ae1ec2
beld78 <belfami@gmail.com>
2017-04-12 15:25:02 +0200

    migrate project

---
+++ src/gui/Logic.java
@@ -0,0 +1,112 @@
+package gui;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Logic {
+
+	List<String[]> groupsList = new LinkedList<>();
+	List<String> participants = new LinkedList<>();
+	
+
+	String textToConvertArray[];
+	int personsInGroup;
+	int amountOfGroups;
+	
+	
+	
+	
+	public String format() {
+
+		createGroups(amountOfGroups, personsInGroup);
+		createParticipants(textToConvertArray);
+		writeParticipantsInGroup(amountOfGroups, personsInGroup);
+		return writeInResultList();
+	}
+
+	private String writeInResultList() {
+		String result = "";
+		for (int i = 0; i < groupsList.size(); i++) {
+			for (int z = 0; z < groupsList.get(i).length; z++) {
+				String currentLine = groupsList.get(i)[z];
+				int removeNumber = currentLine.indexOf(".");
+				if (removeNumber != -1) {
+					currentLine = currentLine.substring(removeNumber + 2);
+				}
+				result += currentLine;
+				result += "\n";
+			}
+		}
+		return result;
+	}
+
+	private void writeParticipantsInGroup(int amountOfGroups, int personsInGroup) {
+		int participantPlace = 0;
+		for (int i = 0; i < personsInGroup; i++) {
+			int startAtEndOfGroup = amountOfGroups - 1;
+			for (int z = 0; z < amountOfGroups; z++) {
+				// go backwards
+				if (!isEven(i)) {
+					groupsList.get(startAtEndOfGroup)[i] = participants.get(participantPlace);
+					startAtEndOfGroup--;
+				} else {
+					groupsList.get(z)[i] = participants.get(participantPlace);
+				}
+				participantPlace++;
+			}
+		}
+	}
+
+	private boolean isEven(int counter) {
+		while (counter > 1) {
+			counter -= 2;
+		}
+		return counter == 0;
+	}
+
+	private void createParticipants(String[] textToConvertArray) {
+		for (String participant : textToConvertArray) {
+			participants.add(participant);
+		}
+
+	}
+
+	private void createGroups(int amountOfGroups, int personsInGroup) {
+		for (int i = 0; i < amountOfGroups; i++) {
+			String[] temp = new String[personsInGroup];
+			groupsList.add(temp);
+		}
+	}
+	
+	public List<String[]> getGroupsList() {
+		return groupsList;
+	}
+	public void setGroupsList(List<String[]> groupsList) {
+		this.groupsList = groupsList;
+	}
+	public List<String> getParticipants() {
+		return participants;
+	}
+	public void setParticipants(List<String> participants) {
+		this.participants = participants;
+	}
+	public String[] getTextToConvertArray() {
+		return textToConvertArray;
+	}
+	public void setTextToConvertArray(String[] textToConvertArray) {
+		this.textToConvertArray = textToConvertArray;
+	}
+	public int getPersonsInGroup() {
+		return personsInGroup;
+	}
+	public void setPersonsInGroup(int personsInGroup) {
+		this.personsInGroup = personsInGroup;
+	}
+	public int getAmountOfGroups() {
+		return amountOfGroups;
+	}
+	public void setAmountOfGroups(int amountOfGroups) {
+		this.amountOfGroups = amountOfGroups;
+	}
+}
---
+++ src/gui/MainWindow.java
@@ -0,0 +1,156 @@
+package gui;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+public class MainWindow {
+
+	protected Shell shlSeedingTool;
+	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
+	private StyledText resultListHere;
+	private Button btnFormat;
+	private Text groups;
+	private Text personsInGroup;
+	private Label lblNewLabel;
+	private Logic logic;
+	private Button btnNewButton;
+
+	/**
+	 * Launch the application.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MainWindow window = new MainWindow();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		logic = new Logic();
+		Display display = Display.getDefault();
+		createContents();
+		shlSeedingTool.open();
+		shlSeedingTool.layout();
+		while (!shlSeedingTool.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shlSeedingTool = new Shell();
+		shlSeedingTool.setSize(371, 287);
+		shlSeedingTool.setText("Seeding Tool");
+		shlSeedingTool.setLayout(new GridLayout(4, false));
+
+		StyledText enterListHere_1 = new StyledText(shlSeedingTool, SWT.BORDER);
+		GridData gd_enterListHere_1 = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 4);
+		gd_enterListHere_1.heightHint = 220;
+		enterListHere_1.setLayoutData(gd_enterListHere_1);
+		enterListHere_1.setText("Enter list here");
+		formToolkit.adapt(enterListHere_1);
+		formToolkit.paintBordersFor(enterListHere_1);
+
+		btnFormat = new Button(shlSeedingTool, SWT.NONE);
+		btnFormat.setImage(SWTResourceManager.getImage(MainWindow.class, "/gui/rober.png"));
+		btnFormat.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 2, 1));
+		btnFormat.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				format(enterListHere_1);
+			}
+
+			private void format(StyledText enterListHere) {
+				String textToConvert = enterListHere.getText();
+				String amountOfGroupsString = groups.getText();
+				String personsInGroupString = personsInGroup.getText();
+
+				String textToConvertArray[] = textToConvert.split("\\r?\\n");
+				int personsInGroup = Integer.parseInt(personsInGroupString);
+				int amountOfGroups = Integer.parseInt(amountOfGroupsString);
+
+				setLogic(textToConvertArray, personsInGroup, amountOfGroups);
+
+				resultListHere.setText(logic.format());
+			}
+
+			private void setLogic(String[] textToConvertArray, int personsInGroup, int amountOfGroups) {
+				logic.setTextToConvertArray(textToConvertArray);
+				logic.setPersonsInGroup(personsInGroup);
+				logic.setAmountOfGroups(amountOfGroups);
+			}
+
+		});
+		formToolkit.adapt(btnFormat, true, true);
+		btnFormat.setText("Click him!");
+
+		resultListHere = new StyledText(shlSeedingTool, SWT.BORDER);
+		resultListHere.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				int end = resultListHere.getCharCount();
+				resultListHere.setSelectionRange(0, end);
+			}
+		});
+		GridData gd_resultListHere = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 4);
+		gd_resultListHere.heightHint = 166;
+		resultListHere.setLayoutData(gd_resultListHere);
+		formToolkit.adapt(resultListHere);
+		formToolkit.paintBordersFor(resultListHere);
+
+		Label lblAmountOfGroups = new Label(shlSeedingTool, SWT.NONE);
+		GridData gd_lblAmountOfGroups = new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1);
+		gd_lblAmountOfGroups.widthHint = 101;
+		lblAmountOfGroups.setLayoutData(gd_lblAmountOfGroups);
+		formToolkit.adapt(lblAmountOfGroups, true, true);
+		lblAmountOfGroups.setText("Amount of Groups");
+
+		groups = new Text(shlSeedingTool, SWT.BORDER);
+		GridData gd_groups = new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1);
+		gd_groups.widthHint = 28;
+		groups.setLayoutData(gd_groups);
+		formToolkit.adapt(groups, true, true);
+
+		lblNewLabel = new Label(shlSeedingTool, SWT.NONE);
+		lblNewLabel.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, false, false, 1, 1));
+		formToolkit.adapt(lblNewLabel, true, true);
+		lblNewLabel.setText("Persons / Group\r\n");
+
+		personsInGroup = new Text(shlSeedingTool, SWT.BORDER);
+		GridData gd_personsInGroup = new GridData(SWT.CENTER, SWT.TOP, false, false, 1, 1);
+		gd_personsInGroup.heightHint = 15;
+		gd_personsInGroup.widthHint = 29;
+		personsInGroup.setLayoutData(gd_personsInGroup);
+		formToolkit.adapt(personsInGroup, true, true);
+
+		btnNewButton = new Button(shlSeedingTool, SWT.NONE);
+		GridData gd_btnNewButton = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
+		gd_btnNewButton.widthHint = 149;
+		btnNewButton.setLayoutData(gd_btnNewButton);
+		formToolkit.adapt(btnNewButton, true, true);
+		btnNewButton.setText("Adjust groups");
+
+	}
+}
