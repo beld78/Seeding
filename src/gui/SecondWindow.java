20f1aa9ee0ab095d652c6ffb7b6f2549dbde806a
beld78 <belfami@gmail.com>
2017-04-18 21:42:31 +0200

    added features
    added WIP color group adjusting (not fledged out) and result as
    snakeform

--- src/gui/SecondWindow.java
+++ src/gui/SecondWindow.java
@@ -1,12 +1,15 @@
 package gui;
 
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -24,6 +27,7 @@ public class SecondWindow {
 	private LinkedList<Table> tableList = new LinkedList<>();
 	private Button swapButton;
 	private StyledText resultList;
+	private StyledText resultListAsSnake;
 	private CCombo colorsCombo;
 	private Label yee;
 
@@ -123,8 +127,19 @@ public class SecondWindow {
 					i++;
 				}
 				System.out.println(toBeSwappedOne);
+				Color colorZero = tableList.get(groupIndices[0]).getItem(playerIndices[0]).getBackground();
+				Color colorOne = tableList.get(groupIndices[1]).getItem(playerIndices[1]).getBackground();
+				swap(playerIndices, groupIndices, toBeSwappedZero, toBeSwappedOne, colorZero, colorOne);
+
+				unselectSelection();
+			}
+
+			private void swap(int[] playerIndices, int[] groupIndices, String toBeSwappedZero, String toBeSwappedOne,
+					Color colorZero, Color colorOne) {
 				tableList.get(groupIndices[0]).getItem(playerIndices[0]).setText(toBeSwappedOne);
 				tableList.get(groupIndices[1]).getItem(playerIndices[1]).setText(toBeSwappedZero);
+				tableList.get(groupIndices[0]).getItem(playerIndices[0]).setBackground(colorOne);
+				tableList.get(groupIndices[1]).getItem(playerIndices[1]).setBackground(colorZero);
 			}
 		});
 
@@ -143,18 +158,30 @@ public class SecondWindow {
 					}
 				}
 				resultList.setText(participants);
+				resultListAsSnake.setText(saveParticipantsInSnakeForm());
 			}
 		});
 
 		resultList = new StyledText(shell, SWT.BORDER);
 		GridData resultListGrid = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 3);
 		resultList.setLayoutData(resultListGrid);
-		resultList.setText("Result will be here");
+		resultList.setText("Result in group form");
 		resultList.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
 				int end = resultList.getCharCount();
 				resultList.setSelectionRange(0, end);
+			}
+		});
+		resultListAsSnake = new StyledText(shell, SWT.BORDER);
+		GridData resultListAsSnakeGrid = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 3);
+		resultListAsSnake.setLayoutData(resultListAsSnakeGrid);
+		resultListAsSnake.setText("Result in snake form");
+		resultListAsSnake.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				int end = resultListAsSnake.getCharCount();
+				resultListAsSnake.setSelectionRange(0, end);
 			}
 		});
 		colorsCombo = new CCombo(shell, SWT.BORDER);
@@ -181,6 +208,7 @@ public class SecondWindow {
 			public void mouseDown(MouseEvent e) {
 				String colorString = colorsCombo.getText();
 				colorPlayers(colorString);
+				unselectSelection();
 			}
 
 			private void colorPlayers(String colorString) {
@@ -190,6 +218,7 @@ public class SecondWindow {
 						TableItem[] tableItems = table.getSelection();
 						for (TableItem tableItem : tableItems) {
 							tableItem.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+							System.out.println(tableItem.getBackground());
 						}
 					}
 				}
@@ -198,6 +227,7 @@ public class SecondWindow {
 						TableItem[] tableItems = table.getSelection();
 						for (TableItem tableItem : tableItems) {
 							tableItem.setBackground(SWTResourceManager.getColor(SWT.COLOR_RED));
+							System.out.println(tableItem.getBackground());
 						}
 					}
 				}
@@ -206,6 +236,7 @@ public class SecondWindow {
 						TableItem[] tableItems = table.getSelection();
 						for (TableItem tableItem : tableItems) {
 							tableItem.setBackground(SWTResourceManager.getColor(SWT.COLOR_BLUE));
+							System.out.println(tableItem.getBackground());
 						}
 					}
 				}
@@ -214,6 +245,7 @@ public class SecondWindow {
 						TableItem[] tableItems = table.getSelection();
 						for (TableItem tableItem : tableItems) {
 							tableItem.setBackground(SWTResourceManager.getColor(SWT.COLOR_YELLOW));
+							System.out.println(tableItem.getBackground());
 						}
 					}
 				}
@@ -222,6 +254,7 @@ public class SecondWindow {
 						TableItem[] tableItems = table.getSelection();
 						for (TableItem tableItem : tableItems) {
 							tableItem.setBackground(SWTResourceManager.getColor(SWT.COLOR_GREEN));
+							System.out.println(tableItem.getBackground());
 						}
 					}
 				}
@@ -230,6 +263,7 @@ public class SecondWindow {
 						TableItem[] tableItems = table.getSelection();
 						for (TableItem tableItem : tableItems) {
 							tableItem.setBackground(SWTResourceManager.getColor(SWT.COLOR_MAGENTA));
+							System.out.println(tableItem.getBackground());
 						}
 					}
 				}
@@ -239,6 +273,7 @@ public class SecondWindow {
 						TableItem[] tableItems = table.getSelection();
 						for (TableItem tableItem : tableItems) {
 							tableItem.setBackground(SWTResourceManager.getColor(SWT.COLOR_CYAN));
+							System.out.println(tableItem.getBackground());
 						}
 					}
 				}
@@ -247,6 +282,119 @@ public class SecondWindow {
 		yee = new Label(shell, SWT.NONE);
 		yee.setText("YEE");
 		yee.setVisible(false);
+
+		Button adjustToColorButton = new Button(shell, SWT.NONE);
+		adjustToColorButton.setText("Adjust to colors");
+
+		adjustToColorButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				saveParticipantsInSnakeForm();
+				LinkedList<String[]> groups = new LinkedList<>();
+				for (Table table : tableList) {
+					String[] colorsOfParticipants = new String[logic.personsInGroup];
+					TableItem[] tableItems = table.getItems();
+					for (int i = 0; i < tableItems.length; i++) {
+						if (tableItems[i].getBackground().toString().equals("Color {255, 255, 255, 255}")) {
+							colorsOfParticipants[i] = ("" + numberInsteadOfColor);
+							numberInsteadOfColor++;
+						} else {
+							colorsOfParticipants[i] = tableItems[i].getBackground().toString();
+						}
+						System.out.println(colorsOfParticipants[i]);
+					}
+					groups.add(colorsOfParticipants);
+				}
+				for (int i = 0; i < groups.size(); i++) {
+					Set<String> oneGroup = new HashSet<>();
+					for (int j = groups.get(i).length - 1; j >= 0; j--) {
+						// duplicate in group check
+						if (!oneGroup.add(groups.get(i)[j])) {
+							String toBeChanged = groups.get(i)[j].toString();
+							if ((i + 1) < groups.size()) {
+								// set bool for check if seed player in this or
+								// different group
+								boolean nextGroupCanBeSwapped = true;
+								for (int k = 0; k < groups.get(j).length; k++) {
+									if (toBeChanged.equals(groups.get(i + 1)[k].toString())) {
+										// seed player in different group
+										nextGroupCanBeSwapped = false;
+									}
+								}
+								// seed player in this group
+								if (nextGroupCanBeSwapped) {
+									seedDown(i, j);
+								} else if (i != 0 && !nextGroupCanBeSwapped) {
+									seedUp(i, j);
+								}
+							} else if (i + 1 == groups.size()) {
+								seedUp(i, j);
+							}
+						}
+					}
+				}
+			}
+
+			private void seedUp(int i, int j) {
+				String switchInOriginalSpot = tableList.get(i).getItem(j).getText();
+				String switchInNewSpot = tableList.get(i - 1).getItem(j).getText();
+				Color bgOriginal = tableList.get(i).getItem(j).getBackground();
+				Color bgNewSpot = tableList.get(i - 1).getItem(j).getBackground();
+				tableList.get(i).getItem(j).setText(switchInNewSpot);
+				tableList.get(i).getItem(j).setBackground(bgNewSpot);
+				tableList.get(i - 1).getItem(j).setText(switchInOriginalSpot);
+				tableList.get(i - 1).getItem(j).setBackground(bgOriginal);
+			}
+
+			private void seedDown(int i, int j) {
+				String switchInOriginalSpot = tableList.get(i + 1).getItem(j).getText();
+				String switchInNewSpot = tableList.get(i).getItem(j).getText();
+				Color bgOriginal = tableList.get(i + 1).getItem(j).getBackground();
+				Color bgNewSpot = tableList.get(i).getItem(j).getBackground();
+				tableList.get(i + 1).getItem(j).setText(switchInNewSpot);
+				tableList.get(i + 1).getItem(j).setBackground(bgNewSpot);
+				tableList.get(i).getItem(j).setText(switchInOriginalSpot);
+				tableList.get(i).getItem(j).setBackground(bgOriginal);
+			}
+		});
 	}
 
+	int numberInsteadOfColor;
+
+	private String saveParticipantsInSnakeForm() {
+		String participants = "";
+		for (int j = 0; j < logic.personsInGroup; j++) {
+			if (isEven(j)) {
+				for (int i = 0; i < logic.amountOfGroups; i++) {
+					participants += tableList.get(i).getItems()[j].getText();
+					participants += "\n";
+				}
+			}
+			if (!isEven(j)) {
+				for (int i = logic.amountOfGroups - 1; i >= 0; i--) {
+					participants += tableList.get(i).getItems()[j].getText();
+					participants += "\n";
+
+				}
+			}
+
+		}
+		return participants;
+	}
+
+	private void unselectSelection() {
+		for (Table table : tableList) {
+			table.deselectAll();
+		}
+	}
+
+	private boolean isEven(int i) {
+		while (i >= 0) {
+			i = i - 2;
+		}
+		if (i == -2) {
+			return true;
+		}
+		return false;
+	}
 }
